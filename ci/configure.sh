d670071817e7d80fd4a90d43cadcb87eab35e131
Belinda Liu <bliu@pivotal.io>
2019-05-10 13:56:05 -0700

    Fix minor errors in pipeline and remove worker tags
    This fixes a few missing inputs and naming errors. It also removes the
    worker tags since we do not have designated workers for bosh-packages.
    
    [#164734079](https://www.pivotaltracker.com/story/show/164734079)
    
    Co-authored-by: Charles Hansen <chansen@pivotal.io>

---
+++ ci/configure.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ $(lpass status -q; echo $?) != 0 ]]; then
+  echo "Login with lpass first"
+  exit 1
+fi
+
+dir=$(dirname $0)
+
+fly -t ${CONCOURSE_TARGET:-production} \
+  sp -p ruby-release \
+  -c $dir/pipeline.yml \
+  -l <(lpass show --notes 'ruby-release pipeline vars')
