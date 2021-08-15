4eeb7c1b33ac7e506c80191535ff957b47d7072b
Joseph Palermo <jpalermo@pivotal.io>
2021-08-15 10:14:14 -0700

    Move pipeline to bosh-ecosystem concourse
    - Add serial group to prevent conflicts when rubygems is bumped

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ $(lpass status -q; echo $?) != 0 ]]; then
-  echo "Login with lpass first"
-  exit 1
-fi
-
 pipeline_config=$(mktemp)
 ytt -f "$(dirname $0)" > $pipeline_config
 
-fly -t ${CONCOURSE_TARGET:-director} \
+fly -t ${CONCOURSE_TARGET:-bosh-ecosystem} \
   sp -p ruby-release \
   -c $pipeline_config \
   -l <(lpass show --notes 'ruby-release pipeline vars')
