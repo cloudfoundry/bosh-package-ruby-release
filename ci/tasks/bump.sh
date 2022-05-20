8bba143849a438340ed7c5a9f2d4ec33aea729c1
Joseph Palermo <jpalermo@pivotal.io>
2022-05-20 14:39:59 -0700

    Remove patch versions from package names
    This will make it easier for releases to auto-bump the minor version of ruby
    they want as the paths to the packages and compile/runtime scripts will be stable
    
    Also removed some interesting "cpi" code that seems like it was treating
    the ruby-release as a cpi for the purposes of tricking the bosh cli
    into compiling it on darwin. Doesn't seem like this was used anymore.
    
    [#182075817]

--- ci/tasks/bump.sh
+++ ci/tasks/bump.sh
@@ -13,12 +13,12 @@ function replace_if_necessary() {
   set +x
   bosh_blobs=$(bosh blobs 2>&1)
   set -x
-  if ! echo $bosh_blobs | grep -q "$blobname"; then
+  if ! echo ${bosh_blobs} | grep -q "${blobname}"; then
     echo "Adding new blob ${blobname}"
     bosh add-blob --sha2 "${blob_location}/${blobname}" "${blobname}"
     bosh upload-blobs
   else
-    echo "Blob $blobname already exists"
+    echo "Blob ${blobname} already exists"
   fi
 }
 
@@ -28,15 +28,14 @@ set +x
 echo "${PRIVATE_YML}" > config/private.yml
 set -x
 
-bosh_release_version=$( cat ../semver/version )
-ruby_blob=$(basename "$(ls ../"ruby"/*)")
-ruby_version="$(cat ../"ruby"/.resource/version)"
-rubygems_blob=$(basename "$(ls ../"rubygems"/*)")
-rubygems_version="$(cat ../"rubygems"/.resource/version)"
+ruby_blob=$(basename "$(ls ../ruby/*)")
+ruby_patch_version="$(cat ../ruby/.resource/version)"
+rubygems_blob=$(basename "$(ls ../rubygems/*)")
+rubygems_version="$(cat ../rubygems/.resource/version)"
 yaml_blob=$(basename "$(ls ../libyaml/*)")
 yaml_version="$(cat ../libyaml/.resource/version)"
-ruby_packagename=${ruby_blob/.tar.gz/}-r${bosh_release_version}
-test_packagename="ruby-$RUBY_VERSION-test"
+ruby_packagename="ruby-${RUBY_VERSION}"
+test_packagename="ruby-${RUBY_VERSION}-test"
 
 if [ "${LIBYAML_VERSION}" != "0.1" ]; then
   if [ ! -e ./src/patches/libyaml-${yaml_version}.patch ]; then
@@ -48,9 +47,9 @@ fi
 echo "-----> $(date): Updating blobs"
 
 bosh blobs
-replace_if_necessary "ruby-$RUBY_VERSION" "$ruby_blob" "../ruby"
-replace_if_necessary "rubygems-$RUBYGEMS_VERSION" "$rubygems_blob" "../rubygems"
-replace_if_necessary "yaml-$LIBYAML_VERSION" "$yaml_blob" "../libyaml"
+replace_if_necessary "ruby-${RUBY_VERSION}" "${ruby_blob}" "../ruby"
+replace_if_necessary "rubygems-${RUBYGEMS_VERSION}" "${rubygems_blob}" "../rubygems"
+replace_if_necessary "yaml-${LIBYAML_VERSION}" "${yaml_blob}" "../libyaml"
 
 echo "-----> $(date): Rendering package and job templates"
 
@@ -58,39 +57,40 @@ echo "-----> $(date): Rendering package and job templates"
 git rm -r packages/ruby-${RUBY_VERSION}* && :
 git rm -r jobs/ruby-${RUBY_VERSION}* && :
 
-mkdir -p "packages/$ruby_packagename"
-mkdir -p "packages/$test_packagename"
-mkdir -p "jobs/$test_packagename/templates"
+mkdir -p "packages/${ruby_packagename}"
+mkdir -p "packages/${test_packagename}"
+mkdir -p "jobs/${test_packagename}/templates"
 
 declare -a template_variables=(
-  "ruby_blob=$ruby_blob"
-  "ruby_version=$ruby_version"
-  "rubygems_blob=$rubygems_blob"
-  "rubygems_version=$rubygems_version"
-  "yaml_blob=$yaml_blob"
-  "yaml_version=$yaml_version"
-  "test_packagename=$test_packagename"
-  "ruby_packagename=$ruby_packagename"
-  "test_jobname=$test_packagename"
+  "ruby_blob=${ruby_blob}"
+  "ruby_version=${RUBY_VERSION}"
+  "ruby_patch_version=${ruby_patch_version}"
+  "rubygems_blob=${rubygems_blob}"
+  "rubygems_version=${rubygems_version}"
+  "yaml_blob=${yaml_blob}"
+  "yaml_version=${yaml_version}"
+  "test_packagename=${test_packagename}"
+  "ruby_packagename=${ruby_packagename}"
+  "test_jobname=${test_packagename}"
 )
 
-erb "${template_variables[@]}" "ci/templates/packages/ruby/spec.erb" > "packages/$ruby_packagename/spec"
-erb "${template_variables[@]}" "ci/templates/packages/ruby/packaging.erb" > "packages/$ruby_packagename/packaging"
+erb "${template_variables[@]}" "ci/templates/packages/ruby/spec.erb" > "packages/${ruby_packagename}/spec"
+erb "${template_variables[@]}" "ci/templates/packages/ruby/packaging.erb" > "packages/${ruby_packagename}/packaging"
+cp "../ruby/.resource/version" "./packages/${ruby_packagename}/"
 
-erb "${template_variables[@]}" "ci/templates/packages/ruby-test/spec.erb" > "packages/$test_packagename/spec"
-erb "${template_variables[@]}" "ci/templates/packages/ruby-test/packaging.erb" > "packages/$test_packagename/packaging"
+erb "${template_variables[@]}" "ci/templates/packages/ruby-test/spec.erb" > "packages/${test_packagename}/spec"
+erb "${template_variables[@]}" "ci/templates/packages/ruby-test/packaging.erb" > "packages/${test_packagename}/packaging"
 
-erb "${template_variables[@]}" "ci/templates/src/compile.env.erb" > "src/compile-$ruby_version.env"
-erb "${template_variables[@]}" "ci/templates/src/runtime.env.erb" > "src/runtime-$ruby_version.env"
+erb "${template_variables[@]}" "ci/templates/src/compile.env.erb" > "src/compile-${RUBY_VERSION}.env"
+erb "${template_variables[@]}" "ci/templates/src/runtime.env.erb" > "src/runtime-${RUBY_VERSION}.env"
 
-erb "${template_variables[@]}" "ci/templates/jobs/ruby-test/monit.erb" > "jobs/$test_packagename/monit"
-erb "${template_variables[@]}" "ci/templates/jobs/ruby-test/spec.erb" > "jobs/$test_packagename/spec"
-erb "${template_variables[@]}" "ci/templates/jobs/ruby-test/templates/cpi.erb" > "jobs/$test_packagename/templates/cpi"
-erb "${template_variables[@]}" "ci/templates/jobs/ruby-test/templates/run.erb" > "jobs/$test_packagename/templates/run"
+erb "${template_variables[@]}" "ci/templates/jobs/ruby-test/monit.erb" > "jobs/${test_packagename}/monit"
+erb "${template_variables[@]}" "ci/templates/jobs/ruby-test/spec.erb" > "jobs/${test_packagename}/spec"
+erb "${template_variables[@]}" "ci/templates/jobs/ruby-test/templates/run.erb" > "jobs/${test_packagename}/templates/run"
 
 for blob in $(bosh blobs | awk '{print $1}')
 do
-  if ! grep -q -R $blob packages; then
+  if ! grep -q -R ${blob} packages; then
     echo "Removing unused blob ${blob}"
     bosh remove-blob "${blob}"
   fi
