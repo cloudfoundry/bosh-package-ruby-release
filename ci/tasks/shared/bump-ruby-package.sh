e127cac2323172122bf16b27cb03f6b80ec637f9
Brian Cunnie <bcunnie@vmware.com>
2023-06-05 16:54:59 -0700

    Add support for updating .ruby-version file
    You can now optionally send a RUBY_VERSION_PATH to the bump-ruby-package
    shared task. When set, the script will update the .ruby-version file to
    contain the version of the Ruby package being used. This path is
    relative to the `bosh-release` input directory. For example, if
    .ruby-version is in the root directory, then you should set:
    ```
    RUBY_VERSION_PATH: .ruby-version
    ```
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- ci/tasks/shared/bump-ruby-package.sh
+++ ci/tasks/shared/bump-ruby-package.sh
@@ -18,11 +18,16 @@ fi
 
 bosh vendor-package ${prefix_arg} ${PACKAGE} "$task_dir/ruby-release"
 
+package_version=$(cat "$task_dir/ruby-release/packages/${PACKAGE}/version")
+
+if [ -n "${RUBY_VERSION_PATH:=}" ]; then
+  echo "${package_version}" > "${RUBY_VERSION_PATH}"
+fi
+
 if [ -z "$(git status --porcelain)" ]; then
   exit
 fi
 
 git add -A
 
-package_version=$(cat "$task_dir/ruby-release/packages/${PACKAGE}/version")
 git commit -m "Update ${PACKAGE} package to ${package_version} from ruby-release"
